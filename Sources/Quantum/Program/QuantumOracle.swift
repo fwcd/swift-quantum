5dd346d56861bd1093c3ed46691d8a809dcf5761
fwcd <fwcd@fwcd.dev>
2024-07-17 00:04:05 +0200

    Add some preconditions around QuantumOracle

--- Sources/Quantum/Program/QuantumOracle.swift
+++ Sources/Quantum/Program/QuantumOracle.swift
@@ -7,6 +7,21 @@ public struct QuantumOracle: Codable, Hashable, LaTeXConvertible {
     /// The truth-table for the classical function represented by this oracle.
     public var values: [[Bool]]
 
+    /// The dimension of the input, i.e. `pow(2, inputBitCount)`
+    public var inputDimension: Int {
+        values.count
+    }
+
+    /// The number of bits in the input.
+    public var inputBitCount: Int {
+        inputDimension.trailingZeroBitCount
+    }
+
+    /// The number of bits in the output.
+    public var outputBitCount: Int {
+        values[0].count
+    }
+
     public var latex: String {
         "O_{\(name)}"
     }
@@ -16,6 +31,10 @@ public struct QuantumOracle: Codable, Hashable, LaTeXConvertible {
         name: String = "f",
         values: [[Bool]]
     ) {
+        precondition(!values.isEmpty, "Quantum oracle values (truth table) must not be empty")
+        precondition(values.count.isPowerOfTwo, "Dimension of quantum oracle values (truth table) is not power of two: \(values.count)")
+        precondition(values.allSatisfy { $0.count == values[0].count }, "Number of output bits must be the same for all inputs: \(values.map(\.count))")
+
         self.type = type
         self.name = name
         self.values = values
