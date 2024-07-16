9edc832ce289ba2e3dbc8f805aa201d0acd02b0e
fwcd <fwcd@fwcd.dev>
2024-07-16 22:27:30 +0200

    Add a bunch of component conformances for convenience

--- Sources/QuantumBuilder/DSL/QuantumProgramComponent.swift
+++ Sources/QuantumBuilder/DSL/QuantumProgramComponent.swift
@@ -5,3 +5,19 @@ public protocol QuantumProgramComponent {
 extension QuantumProgram: QuantumProgramComponent {
     public var program: QuantumProgram { self }
 }
+
+extension Identified<QuantumOperation>: QuantumProgramComponent {
+    public var program: QuantumProgram { QuantumProgram(operations: [self]) }
+}
+
+extension QuantumOperation: QuantumProgramComponent {
+    public var program: QuantumProgram { QuantumProgram(operations: [.init(self)]) }
+}
+
+extension QuantumTransformationExpression: QuantumProgramComponent {
+    public var program: QuantumProgram { QuantumProgram(operations: [.init(.transform(self))]) }
+}
+
+extension Matrix: QuantumProgramComponent {
+    public var program: QuantumProgram { QuantumProgram(operations: [.init(.transform(.matrix(self)))]) }
+}
