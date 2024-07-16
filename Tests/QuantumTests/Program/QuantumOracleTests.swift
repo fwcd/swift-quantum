c06adf67fb5451ebacf1c5862cbcaaf6c82a933a
fwcd <fwcd@fwcd.dev>
2024-07-16 23:35:21 +0200

    Add some more tests for oracle function initialization

---
+++ Tests/QuantumTests/Program/QuantumOracleTests.swift
@@ -0,0 +1,18 @@
+import XCTest
+import Quantum
+
+final class QuantumOracleTests: XCTestCase {
+    func testFunctionInitialization() {
+        // Unfortunately, the lack of same-type constraints in Swift 5.9's variadic generics
+        // poses some problems for type-inference, so we have to specify `Bool` explicitly.
+        // This might get better with https://github.com/swiftlang/swift/pull/70227, with
+        // which we could just replace `BooleanIsomorphic` fully (hopefully?)
+
+        XCTAssertEqual(QuantumOracle { () }.values, [[]])
+        XCTAssertEqual(QuantumOracle { (b: Bool) in b }.values, [[false], [true]])
+        XCTAssertEqual(QuantumOracle { $0 || $1 }.values, [[false], [true], [true], [true]])
+        XCTAssertEqual(QuantumOracle { $0 && $1 }.values, [[false], [false], [false], [true]])
+        XCTAssertEqual(QuantumOracle { (b: Bool, c: Bool) in (b, c) }.values, [[false, false], [false, true], [true, false], [true, true]])
+    }
+}
+
