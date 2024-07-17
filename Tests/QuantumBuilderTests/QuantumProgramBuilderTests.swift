ccbe67b305d06103b96f48a7ee2099b9bef02cc6
fwcd <fwcd@fwcd.dev>
2024-07-17 02:03:29 +0200

    Move the Kronecker program to a unit test

--- Tests/QuantumBuilderTests/QuantumProgramBuilderTests.swift
+++ Tests/QuantumBuilderTests/QuantumProgramBuilderTests.swift
@@ -6,13 +6,30 @@ final class QuantumBuilderTests: XCTestCase {
         assert(QuantumProgram {}, equals: [])
     }
 
-    func testBasic() {
+    func testSimpleProgram() {
         assert(QuantumProgram {
             Hadamard()
             Measure()
         }, equals: [
             .init(.transform(.hadamard)),
             .init(.measure),
+        ])
+    }
+
+    func testSimpleKronExpressions() {
+        assert(QuantumProgram {
+            Kron {
+                Hadamard()
+                Identity().kronPow(2)
+            }
+            Kron {
+                Z()
+                X()
+                X()
+            }
+        }, equals: [
+            .init(.transform(.kron(.hadamard, .kronPow(.identity, 2)))),
+            .init(.transform(.kron(.kron(.z, .x), .x))),
         ])
     }
 }
